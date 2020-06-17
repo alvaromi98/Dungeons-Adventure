e5c8046d25578f22503ad3d5b84a7a8f8ad82ba7
alvaro <javijambo98@gmail.com>
2020-06-17 09:18:25 +0200

    Subido a nuevo repositorio

---
+++ js/display.js
@@ -0,0 +1,79 @@
+const Display = function(canvas) {
+    this.buffer = document.createElement("canvas").getContext("2d");
+    this.context = canvas.getContext("2d");
+    this.volver = document.createElement("button");
+    //MENU SUPERIOR
+    this.volver.innerHTML = "Volver";
+    this.volver.className = "btn btn-info col";
+    this.volver.id = "volver";
+    this.volver.setAttribute("onClick", "window.location.reload();");
+    this.menu = document.createElement("section");
+    this.menu.className = "container-fluid justify-content-center";
+    this.menu.setAttribute("style", "overflow: hidden !important;  margin:0");
+    this.row = document.createElement("article");
+    this.row.className = "row";
+    this.row2 = document.createElement("article");
+    this.row2.className = "row";
+    this.row.setAttribute("style", "overflow: hidden;");
+
+    this.cartelvidas = document.createElement("button");
+    this.cartelvidas.className = "btn col text-white col";
+    this.monedasrestantes = document.createElement("button");
+    this.monedasrestantes.className = "btn col text-white col";
+    document.body.appendChild(this.menu);
+    this.menu.appendChild(this.row);
+    this.menu.appendChild(this.row2);
+    this.row.appendChild(this.cartelvidas);
+    this.row.appendChild(this.volver);
+    this.row.appendChild(this.monedasrestantes);
+    this.row2.appendChild(canvas);
+
+
+    //funcion de dibujar un mapa, recorremos el array y por cada valor seteamos el origena recortar y el destino, después lo dibujamos con dibujarImagen()
+    this.dibujaMapa = function(img, img_col, mapa, m_col, tile_size) {
+            for (let i = mapa.length - 1; i > -1; --i) {
+                //recogemos el valor 
+                var tile = mapa[i];
+                //recogemos el tile
+                var origen_x = (tile % img_col) * tile_size;
+                var origen_y = Math.floor(tile / img_col) * tile_size;
+                //posicion donde vamos a dibujar el tile
+                var destino_x = (i % m_col) * tile_size;
+                var destino_y = Math.floor(i / m_col) * tile_size;
+                //pintamos la tile en el buffer 
+                this.buffer.drawImage(img, origen_x, origen_y, tile_size, tile_size, destino_x, destino_y, tile_size, tile_size);
+            }
+
+        }
+        //funcion de dibujar el personaje 
+    this.dibujaObjeto = function(img, origen_x, origen_y, destino_x, destino_y, width, height) {
+
+        this.buffer.drawImage(img, origen_x, origen_y, width, height, Math.round(destino_x), Math.round(destino_y), width, height);
+    }
+
+    this.resize = function(width, height, ratio) {
+        //para que se reescale correctamente tenemos que comprobar que el ratio de altura : anchura sea el mismo
+        if (height / width > ratio) {
+            this.context.canvas.height = width * ratio;
+            this.context.canvas.width = width;
+
+        } else {
+            this.context.canvas.height = height;
+            this.context.canvas.width = height / ratio;
+        }
+        //para que se vean bien en pixel art y no aparezca borroso tras escalado los pixeles
+        this.context.imageSmoothingEnabled = false;
+
+    }
+
+    this.gameOver = function(img) {
+        this.context.drawImage(img, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height);
+    }
+}
+
+Display.prototype = {
+    constructor: Display,
+    render: function() {
+        this.context.drawImage(this.buffer.canvas, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height);
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,125 @@
+function start() {
+
+    //quitamos el header y el body para dar espacio al canvas
+    document.getElementById("header").remove();
+    document.getElementById("main").remove();
+    var elem = document.getElementsByTagName("footer")[0];
+    elem.parentNode.removeChild(elem);
+
+    var keyDownUp = function(event) {
+            controller.keyDownUp(event.type, event.keyCode);
+        }
+        //reescalado
+    var resize = function() {
+
+            display.resize(document.documentElement.clientWidth, document.documentElement.clientHeight, game.world.height / game.world.width);
+            display.render();
+
+        }
+        //renderizado
+    var render = function() {
+            //dibujamos el mapa
+            display.dibujaMapa(loader.tile_set_image, game.world.tile_set.columnas, game.world.mapa, game.world.columnas, game.world.tile_set.tile_size);
+            //recogemos los frames para la animacion
+            let frame_personaje = game.world.tile_set.array_frames[game.world.personaje.f_value];
+            //dibujamos el personaje con los frames y la posicion correspondientes
+            display.dibujaObjeto(loader.tile_set_image, frame_personaje.x, frame_personaje.y, game.world.personaje.x, game.world.personaje.y, frame_personaje.width, frame_personaje.height);
+            for (let i = 0; i < game.world.coins.length; i++) {
+                let coin = game.world.coins[i];
+                let framecoin = game.world.tile_set.array_frames[coin.f_value];
+                display.dibujaObjeto(loader.tile_set_image, framecoin.x, framecoin.y, coin.x, coin.y, framecoin.width, framecoin.height);
+            }
+            for (let k = 0; k < game.world.spikes.length; k++) {
+                let sierra = game.world.spikes[k];
+                let f = game.world.tile_set.array_frames[sierra.f_value];
+                display.dibujaObjeto(loader.tile_set_image, f.x, f.y, sierra.x, sierra.y, f.width, f.height);
+            }
+            for (let k = 0; k < game.world.blocks.length; k++) {
+                let block = game.world.blocks[k];
+                let f = game.world.tile_set.array_frames[block.f_value];
+                display.dibujaObjeto(loader.tile_set_image, f.x, f.y, block.x, block.y, f.width, f.height);
+            }
+            for (let k = 0; k < game.world.platforms.length; k++) {
+                let platform = game.world.platforms[k];
+                let f = game.world.tile_set.array_frames[platform.f_value];
+                display.dibujaObjeto(loader.tile_set_image, f.x, f.y, platform.x, platform.y, f.width, f.height);
+            }
+            display.render();
+        }
+        //update, setea el movimiento del personaje al que es dependiendo de que tecla hayamos pulsado
+    var update = function() {
+        if (controller.izq.active) {
+            game.world.personaje.moverIzq();
+        }
+        if (controller.dcha.active) {
+            game.world.personaje.moverDcha();
+        }
+        if (controller.saltar.active) {
+            game.world.personaje.saltar();
+            controller.saltar.active = false;
+        }
+        if (game.world.personaje.vidas < 0) {
+            loader.rqTileImage("tiles/gameover.png", (imagen) => {
+                display.gameOver(imagen);
+                engine.stop();
+            })
+        }
+
+        //añadimos el cartel de  vidas restantes
+        if (game.world.personaje.vidas >= -1) {
+            display.cartelvidas.innerHTML = "Vidas restantes: " + "&#10084;".repeat(game.world.personaje.vidas + 1);
+        }
+        if (game.world.score >= -1) {
+            display.monedasrestantes.innerHTML = "Monedas restantes: " + "<img src='tiles/coin.png' width='18px' height='18px'> ".repeat(game.world.score);
+        }
+        game.update();
+        //si el personaje colisiona con una puerta inizializa la variable gate la cual indica que nivel cargar
+        if (game.world.gate) {
+            //paramos el juego
+            engine.stop();
+            //recogemos el nuevo nivel y lo cargamos
+            loader.rqJSON("json/nivel" + game.world.gate.nivel_destino + ".json", (nivel) => {
+                //cargamos el nivel
+                game.world.cargarNivel(nivel);
+                //iniciamos el juego de nuevo
+                engine.start();
+
+            });
+            return;
+        }
+
+    }
+
+    //inicializar
+    var loader = new Loader();
+    var controller = new Controller();
+    var game = new Game();
+    var display = new Display(document.querySelector("canvas"));
+    var engine = new Engine(1000 / 30, render, update);
+
+    //cargar    
+    display.buffer.canvas.height = game.world.height;
+    display.buffer.canvas.width = game.world.width;
+    display.buffer.imageSmoothingEnabled = false;
+
+    loader.rqJSON("json/nivel" + game.world.id_nivel + ".json", (nivel) => {
+        game.world.cargarNivel(nivel);
+
+        loader.rqTileImage("tiles/tiles.png", (imagen) => {
+            loader.tile_set_image = imagen;
+            resize();
+            engine.start();
+        })
+    })
+
+
+    window.addEventListener("keydown", keyDownUp);
+    window.addEventListener("keyup", keyDownUp);
+    window.addEventListener("keydown", function(e) {
+        // space and arrow keys
+        if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+        }
+    }, false)
+    window.addEventListener("resize", resize);
+};
